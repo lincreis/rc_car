5423bb63b5fca1694773d5c7dba18b5bfe83f559
Lincoln Reis <cmte.lincoln@gmail.com>
2025-03-01 22:02:06 -0500

    Update file

--- uptade.sh
+++ uptade.sh
@@ -3,6 +3,10 @@
 # Pull latest changes from GitHub
 cd ~
 git pull origin main
+git stash         # Stash (hide) your local changes
+git pull origin main  # Pull latest changes from remote
+git stash pop     # Reapply your local changes
+
 
 # Check if package-lock.json changed
 if git diff-tree --name-only HEAD^ HEAD | grep -q "package-lock.json"; then
@@ -15,4 +19,3 @@ sudo chmod +x robot_pi.py web_server.py
 # Reboot
 echo "Reboot in 5 seconds"
 sleep 5
-sudo reboot
