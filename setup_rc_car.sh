2bef160c9e8a77329005a580d156b0ec5053f420
Lincoln Reis <cmte.lincoln@gmail.com>
2025-03-06 22:56:47 -0500

    Update installation files

--- setup_rc_car.sh
+++ setup_rc_car.sh
@@ -4,23 +4,26 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install required packages
-sudo apt install -y python3-pip python3-venv git libatlas-base-dev pigpio python3-picamera
+sudo apt install -y python3-pip python3-venv python3-dev libboost-python-dev python3-git libatlas-base-dev pigpio python3-picamera2
 
 # Start pigpio daemon
 sudo systemctl enable pigpiod
 sudo systemctl start pigpiod
 
-# Create directory structure
-mkdir -p ~/rc_car
-cd ~/rc_car
+# Enable camera and SPI
+sudo raspi-config nonint do_camera 0
+sudo raspi-config nonint do_spi 0
 
 # Clone repository
 git clone https://github.com/lincreis/rc_car.git
 cd rc_car
 
 # Create virtual environments
+mkdir env
+cd ~/rc_car/env
 python3 -m venv car_venv
 python3 -m venv web_venv
+python3 -m venv nrf24_env
 
 # Activate and install dependencies for car control
 source car_venv/bin/activate
@@ -31,48 +34,17 @@ deactivate
 # Activate and install dependencies for web server
 source web_venv/bin/activate
 pip install --upgrade pip
-pip install picamera
+pip install pyrf24 spidev
+deactivate
+
+# Activate and install dependencies for radio
+source nrf24_venv/bin/activate
+pip install --upgrade pip
+pip install picamera2
 deactivate
 
 # Make scripts executable
-chmod +x car_control.py web_server.py
-
-# Create systemd services
-cat << EOF | sudo tee /etc/systemd/system/rc_car_control.service
-[Unit]
-Description=RC Car Control Service
-After=network.target
-
-[Service]
-ExecStart=/home/pi/rc_car/rc_car/car_venv/bin/python3 /home/pi/rc_car/rc_car/car_control.py
-WorkingDirectory=/home/pi/rc_car/rc_car
-Restart=always
-User=pi
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-cat << EOF | sudo tee /etc/systemd/system/rc_car_web.service
-[Unit]
-Description=RC Car Web Service
-After=network.target
-
-[Service]
-ExecStart=/home/pi/rc_car/rc_car/web_venv/bin/python3 /home/pi/rc_car/rc_car/web_server.py
-WorkingDirectory=/home/pi/rc_car/rc_car
-Restart=always
-User=pi
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# Enable and start services
-sudo systemctl enable rc_car_control.service
-sudo systemctl enable rc_car_web.service
-sudo systemctl start rc_car_control.service
-sudo systemctl start rc_car_web.service
+chmod +x car_control.py web_server.py joystick.py
 
 echo "Setup complete! Access the web interface at http://<raspberry_pi_ip>:8000"
 echo "Rebooting in 5 seconds..."
